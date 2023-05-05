54339d60074ec8f201491ac2589b9571810db42f
faizanulhassan32 <faizanulhassan043@gmail.com>
2023-05-05 22:11:35 +0500

    2nd model used

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import json
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 df = pd.read_csv("Dataset.csv")
@@ -11,11 +11,11 @@ train_data, test_data = train_test_split(df, test_size=0.2, random_state=42)
 # Define the features and target variables
 features = ['Open', 'High', 'Low', 'Volume_(BTC)', 'Volume_(Currency)']
 target = 'Weighted_Price'
-# Train the decision tree regressor
-tree = DecisionTreeRegressor(max_depth=5, random_state=42)
-tree.fit(train_data[features], train_data[target])
+# Train the random forest regressor
+rf = RandomForestRegressor(n_estimators=8, max_depth=2, random_state=42)
+rf.fit(train_data[features], train_data[target])
 # Evaluate the model on the testing set
-y_pred = tree.predict(test_data[features])
+y_pred = rf.predict(test_data[features])
 mse = mean_squared_error(test_data[target], y_pred)
 mae = mean_absolute_error(test_data[target], y_pred)
 # Save the metrics to a JSON file using DVC
